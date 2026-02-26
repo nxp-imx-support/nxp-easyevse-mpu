99d10237cf57a461f738bc3a79fbf27916c5ba54
Lalit Saini <lalit.saini@nxp.com>
2026-02-26 18:02:02 +0530

    IIOTSOL1-1296: fix: update generated folder path to development directory
    Replace generated folder reference with development directory to resolve
    regeneration issues on Yocto build system.
    
    Signed-off-by: Lalit Saini <lalit.saini@nxp.com>

--- generated/guider_fonts/lv_font_montserratMedium_16.c
+++ generated/guider_fonts/lv_font_montserratMedium_16.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 NXP
+ * Copyright 2026 NXP
  * NXP Confidential and Proprietary. This software is owned or controlled by NXP and may only be used strictly in
  * accordance with the applicable license terms. By expressly accepting such terms or by downloading, installing,
  * activating and/or otherwise using the software, you are agreeing that you have read, and that you agree to
@@ -9,7 +9,7 @@
 /*******************************************************************************
  * Size: 16 px
  * Bpp: 4
- * Opts: --enable-crashpad --crashpad-handler-pid=1174540 --enable-crash-reporter=3552b632-af5a-44db-9c84-0b02a23f9a95,no_channel --user-data-dir=/home/lalit/.config/gui-guider --app-path=/opt/Gui-Guider/resources/app.asar --no-sandbox --no-zygote --disable-gpu-compositing --lang=en-US --num-raster-threads=4 --enable-main-frame-before-activation --renderer-client-id=6 --time-ticks-at-unix-epoch=-1758083439557432 --launch-time-ticks=7442271618242 --shared-files=v8_context_snapshot_data:100 --field-trial-handle=0,i,15013407311288179253,4874519749695990531,131072 --disable-features=SpareRendererForSitePerProcess --enable-crashpad
+ * Opts: --enable-crashpad --crashpad-handler-pid=1803130 --enable-crash-reporter=3552b632-af5a-44db-9c84-0b02a23f9a95,no_channel --user-data-dir=/home/lalit/.config/gui-guider --app-path=/opt/Gui-Guider/resources/app.asar --no-sandbox --no-zygote --disable-gpu-compositing --lang=en-US --num-raster-threads=4 --enable-main-frame-before-activation --renderer-client-id=6 --time-ticks-at-unix-epoch=-1758083439557432 --launch-time-ticks=12791286993273 --shared-files=v8_context_snapshot_data:100 --field-trial-handle=0,i,5418888734815112482,14168596274884660104,131072 --disable-features=SpareRendererForSitePerProcess --enable-crashpad
  ******************************************************************************/
 
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
