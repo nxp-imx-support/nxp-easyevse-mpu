e169114c2d085cc72a80ede1b8b6afc127a49b1b
Lalit Saini <lalit.saini@nxp.com>
2026-03-30 16:29:43 +0530

    IIOTSOL1-1475: Rearrange files into correct folder structure
        What is changed:
        - Moved image asset files to their correct directory
        - Moved font files to the appropriate fonts folder
        - Adjusted related paths to align with the expected project structure
    
        Why it is changed:
        - Some generated files were previously placed in incorrect folders
        - Proper organization is required for consistent build output and maintainability
    
        Technical context:
        - No modifications to file content
        - Only relocation of files to correct paths based on the GUI generator’s output structure
    
    Signed-off-by: Lalit Saini <lalit.saini@nxp.com>

--- generated/guider_fonts/lv_font_arial_20.c
+++ generated/guider_fonts/lv_font_arial_20.c
@@ -9,7 +9,7 @@
 /*******************************************************************************
  * Size: 20 px
  * Bpp: 4
- * Opts: --enable-crashpad --crashpad-handler-pid=1803130 --enable-crash-reporter=3552b632-af5a-44db-9c84-0b02a23f9a95,no_channel --user-data-dir=/home/lalit/.config/gui-guider --app-path=/opt/Gui-Guider/resources/app.asar --no-sandbox --no-zygote --disable-gpu-compositing --lang=en-US --num-raster-threads=4 --enable-main-frame-before-activation --renderer-client-id=6 --time-ticks-at-unix-epoch=-1758083439557432 --launch-time-ticks=12791286993273 --shared-files=v8_context_snapshot_data:100 --field-trial-handle=0,i,5418888734815112482,14168596274884660104,131072 --disable-features=SpareRendererForSitePerProcess --enable-crashpad
+ * Opts: --enable-crashpad --crashpad-handler-pid=624344 --enable-crash-reporter=3552b632-af5a-44db-9c84-0b02a23f9a95,no_channel --user-data-dir=/home/lalit/.config/gui-guider --app-path=/opt/Gui-Guider/resources/app.asar --no-sandbox --no-zygote --disable-gpu-compositing --lang=en-US --num-raster-threads=4 --enable-main-frame-before-activation --renderer-client-id=6 --time-ticks-at-unix-epoch=-1758083439557433 --launch-time-ticks=15817058389199 --shared-files=v8_context_snapshot_data:100 --field-trial-handle=0,i,8139009689389938758,5189580750099233934,131072 --disable-features=SpareRendererForSitePerProcess --enable-crashpad
  ******************************************************************************/
 
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
